58c1aa25592e3ed5201a306b03e2de31295f8c3f
Shane Nguyen <shane.sn911@gmail.com>
2026-03-13 21:43:51 -0700

    c

--- functions.cpp
+++ functions.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include "headers.h"
 
 using namespace std;
 
--- newanagram.cpp
+++ newanagram.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+#include "functions.cpp"
 #include "headers.h"
 
 int main() {
